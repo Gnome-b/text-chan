22e640ae33461181c6a805ebf5c667f156382bf9
Gnome <usenko.toly@gmail.com>
2026-09-08 12:02:16 +0300

    feat: add post get functionality

--- src/crud/crud_post.py
+++ src/crud/crud_post.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from database import DbDependency
 from models.post import PostModel
 from schemas.post import PostCreate
@@ -16,3 +18,8 @@ async def create_post(post_data: PostCreate, db: DbDependency) -> PostModel:
     await db.commit()
     await db.refresh(new_post)
     return new_post
+
+
+async def get_post(post_id: int, db: DbDependency) -> PostModel | None:
+    result = await db.execute(select(PostModel).where(PostModel.id == post_id))
+    return result.scalar_one_or_none()
