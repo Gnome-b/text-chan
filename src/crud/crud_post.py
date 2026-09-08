af36a46a9e41f38b61b23096fdb708f8f995ffb6
Gnome <usenko.toly@gmail.com>
2026-09-08 18:15:20 +0300

    feat: add posts get functionality

--- src/crud/crud_post.py
+++ src/crud/crud_post.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import desc, select
 
 from database import DbDependency
 from models.post import PostModel
@@ -23,3 +23,14 @@ async def create_post(post_data: PostCreate, db: DbDependency) -> PostModel:
 async def get_post(post_id: int, db: DbDependency) -> PostModel | None:
     result = await db.execute(select(PostModel).where(PostModel.id == post_id))
     return result.scalar_one_or_none()
+
+
+async def get_posts(page: int, limit: int, db: DbDependency) -> list[PostModel]:
+    offset = (page - 1) * limit
+    result = await db.execute(
+        select(PostModel)
+        .order_by(desc(PostModel.created_at))
+        .offset(offset)
+        .limit(limit)
+    )
+    return result.scalar().all()
