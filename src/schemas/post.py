20dabe02b6d372119af1638461eaf1e0996cfbbb
Gnome <usenko.toly@gmail.com>
2026-09-09 13:01:05 +0300

    feat: add PostUpdate class

--- src/schemas/post.py
+++ src/schemas/post.py
@@ -2,11 +2,13 @@ from datetime import datetime
 
 from pydantic import BaseModel, Field
 
+
 class PostCreate(BaseModel):
     author_name: str | None = Field(default=None, max_length=50)
     subject: str | None = Field(default=None, max_length=100)
     text: str = Field(min_length=1, max_length=5000)
     secret_code: str = Field(min_length=4, max_length=100)
+
 
 class PostResponse(BaseModel):
     id: int
@@ -14,3 +16,9 @@ class PostResponse(BaseModel):
     subject: str | None
     text: str
     created_at: datetime
+
+
+class PostUpdate(BaseModel):
+    author_name: str | None = Field(default=None, max_length=50)
+    subject: str | None = Field(default=None, max_length=100)
+    text: str | None = Field(default=None, min_length=1, max_length=5000)
